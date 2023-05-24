3f4414109d42ff6fb5f5af0219aa9135c2aa280a
AmitkrMeena02 <amitmeena.meena@yahoo.com>
2023-05-24 17:44:30 +0530

    added filter

--- dom.js
+++ dom.js
@@ -98,7 +98,7 @@ let newultext = document.createTextNode('HEllo'); // Create text node
 newul.appendChild(newultext); // Add text to div
 
 let itemTitle = document.querySelector(`#items .list-group-item:first-child`);
-itemTitle.parentNode.insertBefore(newul, itemTitle); */
+itemTitle.parentNode.insertBefore(newul, itemTitle); 
 
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
@@ -152,4 +152,26 @@ function removeItem(e) {
       itemList.removeChild(li);
     }
   }
+} */
+
+var filter = document.getElementById('filter');
+
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
+// Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
 }
